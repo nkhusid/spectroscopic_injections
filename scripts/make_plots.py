ccf8cf74a96731c55fe5020f47151a2141108ebc
Nicole Khusid <nicole.khusid@stonybrook.edu>
2026-06-09 15:57:04 -0400

    plots for dfpre = -0.5 results

--- scripts/make_plots.py
+++ scripts/make_plots.py
@@ -15,7 +15,13 @@ def main():
 
     parser.add_argument('--path', required=True, help='Relative path to the directory containing the results for the injection, e.g. "DS/220".')
 
-    parser.add_argument('--suffix', required=False, default=False, nargs='+', help='Specify specific injections for which to generate plots.')
+    parser.add_argument('--suffix', required=False, default=False, nargs='+', help='Specify injections for which to generate plots.')
+
+    parser.add_argument('--mchi', required=False, default=True, help='Generate mchi plot. Defaults to True.')
+
+    parser.add_argument('--amps', required=False, default=True, help='Generate amplitude decay plot. Defaults to True.')
+
+    parser.add_argument('--comp', required=False, default=True, help='Generate LOO plot. Defaults to True.')
 
     # parser.add_argument('--ringup', required=False, default='equal', help='Morphology for the ringup of the ')
 
@@ -76,94 +82,97 @@ def main():
                     print(e)
 
             ### MCHI SUMMARY PLOT ###
-            print('Making mchi plots...')
-            fig, ax = plt.subplots(1, len(dfs), figsize=(11*1.5, 5), sharex=True, sharey=True)
-            if len(dfs) == 1:
-                ax = [ax]
+            if args.mchi:
+                print('Making mchi plots...')
+                fig, ax = plt.subplots(1, len(dfs), figsize=(11*1.5, 5), sharex=True, sharey=True)
+                if len(dfs) == 1:
+                    ax = [ax]
 
-            for i, (combo, df) in enumerate(dfs.items()):
-                legend = True
-                if i != 0:
-                    legend = False
-                plot_mfcf_man(df, m, chi, ax[i], legend=legend, **dict(palette='Oranges_r'))
-                ax[i].set_title(f'QNM model: {combo}')
+                for i, (combo, df) in enumerate(dfs.items()):
+                    legend = True
+                    if i != 0:
+                        legend = False
+                    plot_mfcf_man(df, m, chi, ax[i], legend=legend, **dict(palette='Oranges_r'))
+                    ax[i].set_title(f'QNM model: {combo}')
 
-            fig.suptitle(f'DS injection: Kerr {combo_true}')
+                fig.suptitle(f'DS injection: Kerr {combo_true}')
 
-            plt.savefig(str(mchi_figpath), bbox_inches='tight')
+                plt.savefig(str(mchi_figpath), bbox_inches='tight')
 
             ### AMPS SUMMARY PLOT ###
-            print('Making amplitude plots...')
-            tref = 0
-            a_true_df = {mode: get_projection(dfs[combo_true], mode, tref) for mode in modes_true}
+            if args.amps:
+                print('Making amplitude plots...')
+                tref = 0
+                a_true_df = {mode: get_projection(dfs[combo_true], mode, tref) for mode in modes_true}
 
-            fig, ax = plt.subplots(len(modes_true), 1, figsize=(8, 4), layout='constrained', sharex=True)
-            a_scale = 1e-21
-            clevs = [0.9, 0.5]
+                fig, ax = plt.subplots(len(modes_true), 1, figsize=(8, 4), layout='constrained', sharex=True)
+                a_scale = 1e-21
+                clevs = [0.9, 0.5]
 
-            if type(ax) is not np.ndarray:
-                ax = [ax]
+                if type(ax) is not np.ndarray:
+                    ax = [ax]
 
-            ax[len(modes_true)-1].set_xlabel('$t_> = t - t_{\\mathrm{peak}}$ [$t_{M_f}$]', fontsize=18)
-            ax[len(modes_true)-1].set_xticks(np.arange(-12, 15, 3))
+                ax[len(modes_true)-1].set_xlabel('$t_> = t - t_{\\mathrm{peak}}$ [$t_{M_f}$]', fontsize=18)
+                ax[len(modes_true)-1].set_xticks(np.arange(-12, 15, 3))
 
-            c = sns.color_palette('Oranges', n_colors=1)[0]
+                c = sns.color_palette('Oranges', n_colors=1)[0]
 
-            for i, mode in enumerate(modes_true):
-                plot_projection(a_true_df[mode], clevs, ax[i])
+                for i, mode in enumerate(modes_true):
+                    plot_projection(a_true_df[mode], clevs, ax[i])
 
-            legend_handles = []
-            for mark, (combo, df) in enumerate(dfs.items()):
+                legend_handles = []
+                for mark, (combo, df) in enumerate(dfs.items()):
 
-                for i, mode in enumerate(combo.split('+')):
-                    if i < len(modes_true):
-                        plot_scan(df, mode, clevs, c, ax[i], marker=filled[mark+1], shift=np.linspace(-0.15, 0.15, len(dfs))[mark])
-                        ax[i].set_ylabel(f'$A_{{{i}}}$ [$10^{{{int(np.log10(a_scale))}}}$]', fontsize=18)
-                        ax[i].set_xlim(-12.5, 12.5)
-                        ax[i].set_ylim(0, 10)
-                        ax[i].tick_params(axis='both', labelsize=16, direction='in')
+                    for i, mode in enumerate(combo.split('+')):
+                        if i < len(modes_true):
+                            plot_scan(df, mode, clevs, c, ax[i], marker=filled[mark+1], shift=np.linspace(-0.15, 0.15, len(dfs))[mark])
+                            ax[i].set_ylabel(f'$A_{{{i}}}$ [$10^{{{int(np.log10(a_scale))}}}$]', fontsize=18)
+                            ax[i].set_xlim(-12.5, 12.5)
+                            ax[i].set_ylim(0, 10)
+                            ax[i].tick_params(axis='both', labelsize=16, direction='in')
+                    
+                    legend_handles.append(Line2D([0, 0], [0, 1], color='k', marker=filled[mark+1], 
+                                                    linestyle='-', linewidth=2, label='Kerr '+combo, 
+                                                    markerfacecolor='k', markeredgecolor='k'))
                 
-                legend_handles.append(Line2D([0, 0], [0, 1], color='k', marker=filled[mark+1], 
-                                                linestyle='-', linewidth=2, label='Kerr '+combo, 
-                                                markerfacecolor='k', markeredgecolor='k'))
-            
-            leg = ax[0].legend(handles=legend_handles, loc=(0.47, 0.52), frameon=False, ncol=2, fontsize=13)
-            
-            plt.savefig(str(amps_figpath), bbox_inches='tight')
+                leg = ax[0].legend(handles=legend_handles, loc=(0.47, 0.52), frameon=False, ncol=2, fontsize=13)
+                
+                plt.savefig(str(amps_figpath), bbox_inches='tight')
 
             ### LOO SUMMARY PLOT ###
-            print('Making LOO comparison plot...')
+            if args.comp:
+                print('Making LOO comparison plot...')
 
-            loo_colls = copy.deepcopy(colls)
+                loo_colls = copy.deepcopy(colls)
 
-            for combo, coll in loo_colls.items():
-                coll.reindex_by_t0(reference_mass=m, reference_time=t0, decimals=1)
+                for combo, coll in loo_colls.items():
+                    coll.reindex_by_t0(reference_mass=m, reference_time=t0, decimals=1)
 
-            loo_dict = pd.DataFrame({combo: [r.idx[s].loo.elpd_loo for s in dfs[combo]['run'].unique()] for combo, r in loo_colls.items()}, index=np.array(dfs[list(dfs.keys())[0]]['run'].unique()))
+                loo_dict = pd.DataFrame({combo: [r.idx[s].loo.elpd_loo for s in dfs[combo]['run'].unique()] for combo, r in loo_colls.items()}, index=np.array(dfs[list(dfs.keys())[0]]['run'].unique()))
 
-            model_dfs = []
-            for s in loo_dict.index:
-                model_df = az.compare({k: r.idx[s] for k, r in loo_colls.items()}, ic='loo', var_name='whitened_pointwise_loglike')
-                model_df['$t_> = t - t_{\\mathrm{peak}}$ [$t_{M_f}$]'] = s
-                model_dfs.append(model_df)
+                model_dfs = []
+                for s in loo_dict.index:
+                    model_df = az.compare({k: r.idx[s] for k, r in loo_colls.items()}, ic='loo', var_name='whitened_pointwise_loglike')
+                    model_df['$t_> = t - t_{\\mathrm{peak}}$ [$t_{M_f}$]'] = s
+                    model_dfs.append(model_df)
 
-            comp_df = pd.concat(model_dfs, ignore_index=False)
-            comp_df['model'] = comp_df.index
-            comp_df['elpd_diff_doub'] = comp_df['elpd_diff'] * 2
-            comp_df['dse_doub'] = comp_df['dse'] * 2
+                comp_df = pd.concat(model_dfs, ignore_index=False)
+                comp_df['model'] = comp_df.index
+                comp_df['elpd_diff_doub'] = comp_df['elpd_diff'] * 2
+                comp_df['dse_doub'] = comp_df['dse'] * 2
 
-            fig, ax = plt.subplots()
-            sns.scatterplot(comp_df, x='$t_> = t - t_{\\mathrm{peak}}$ [$t_{M_f}$]', y='elpd_diff_doub', hue='model', s=70, ax=ax)
-            for i in range(len(comp_df)):
-                if comp_df.iloc[i]['rank'] != 0:
-                    ax.errorbar(x=comp_df.iloc[i]['$t_> = t - t_{\\mathrm{peak}}$ [$t_{M_f}$]'], y=comp_df.iloc[i]['elpd_diff_doub'], yerr=comp_df.iloc[i]['dse_doub'], 
-                                lw=0, elinewidth=2, c=f'C{list(comp_df.index.unique()).index(comp_df.index[i])}', capsize=7, capthick=2, zorder=-1)
-            ax.set_ylabel('$2 \\times \Delta \mathrm{ELPD} \sim |\Delta \chi^2|$')
-            ax.axhline(0, c='gray', ls='--', zorder=-1)
-            ax.grid(alpha=0.2)
-            ax.set_yticks(np.arange(0, 10))
-            ax.set_ylim(9, -1)
-            plt.savefig(str(comp_figpath), bbox_inches='tight')
+                fig, ax = plt.subplots()
+                sns.scatterplot(comp_df, x='$t_> = t - t_{\\mathrm{peak}}$ [$t_{M_f}$]', y='elpd_diff_doub', hue='model', s=70, ax=ax)
+                for i in range(len(comp_df)):
+                    if comp_df.iloc[i]['rank'] != 0:
+                        ax.errorbar(x=comp_df.iloc[i]['$t_> = t - t_{\\mathrm{peak}}$ [$t_{M_f}$]'], y=comp_df.iloc[i]['elpd_diff_doub'], yerr=comp_df.iloc[i]['dse_doub'], 
+                                    lw=0, elinewidth=2, c=f'C{list(comp_df.index.unique()).index(comp_df.index[i])}', capsize=7, capthick=2, zorder=-1)
+                ax.set_ylabel('$2 \\times \Delta \mathrm{ELPD} \sim |\Delta \chi^2|$')
+                ax.axhline(0, c='gray', ls='--', zorder=-1)
+                ax.grid(alpha=0.2)
+                ax.set_yticks(np.arange(0, 10))
+                ax.set_ylim(9, -1)
+                plt.savefig(str(comp_figpath), bbox_inches='tight')
 
 
 if __name__ == "__main__":
