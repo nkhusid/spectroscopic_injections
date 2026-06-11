c303e5e48c8b77177dfc113b2f0f6f7d5c604ca6
Nicole Khusid <nicole.khusid@stonybrook.edu>
2026-06-11 14:26:04 -0400

    adding LVCNR .h5 files

--- scripts/directories.py
+++ scripts/directories.py
@@ -4,6 +4,8 @@ condir = Path('/mnt/home/nkhusid/spec_inj/configs')
 
 datdir = Path('/mnt/home/nkhusid/spec_inj/data/injections')
 
+sxsdir = Path('/mnt/home/nkhusid/spec_inj/data/sxs_sims')
+
 resdir = Path('/mnt/home/nkhusid/ceph/spec_inj')
 
 figdir = Path('/mnt/home/nkhusid/spec_inj/figs')
